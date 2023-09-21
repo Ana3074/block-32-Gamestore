cd61238773cc80298778a6dd9cea76886c6aceb0
Ana Carolina <92136409+Ana3074@users.noreply.github.com>
2023-09-21 19:38:02 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+// Prompt 1 Get all video games by ID.
+// Write a function that gets a video game by ID from the database.
+// getAllVideoGames() function.
+
+// Prompt 2 Get all video games.
+// Write a function that gets a video game by ID from the database.
+// getVideoGameById() function.
+
+// Prompt 3 POST create a video game
+// Write a function that creates a video game in the database.
+// createVideoGame() function.
+
+// Prompt 4 PUT update video game
+// Write a function that updates a video game in the database.
+// Use the updateVideoGame() function.
+
+// Prompt 5 DELETE video game
+// Write an asynchronous database query function that deletes a video game from the database.
+// Use the deleteVideoGame() function.
