2c240cf8e53fa30f38e1fd84e9128ecdcc289138
Ana Carolina <92136409+Ana3074@users.noreply.github.com>
2023-09-21 19:35:44 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-const express = require('express');
-const router = express.Router();
-
-// GET /api/health
-router.get('/health', (req, res, next) => {
-    res.send('OK');
-});
-
-// ROUTER: /api/video-games
-router.use('/video-games', require('./videoGames'));
-
-// ROUTER: /api/board-games
-router.use('/board-games', require('./boardGames'));
-
-module.exports = router;
+module.exports = {
+    ...require('./videoGames'),
+    ...require('./boardGames')
+};
